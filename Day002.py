ed765355f38dc48a9d1fe29a3dd76c2d9f6aac8e
ALharthi, Fatmah M <48739238+Fatmahmh@users.noreply.github.com>
2019-08-24 12:02:12 +0300

    Create Day002.py

---
+++ Day002.py
@@ -0,0 +1,16 @@
+#this is a comment
+print("hello world!")
+
+print("hello world!") #this is a comment
+
+#print("hello world!")
+print("hello world!")
+
+#this is comment
+#written in more than one line
+print("hello world!")
+
+''' this is comment
+written in more than one line
+'''
+print("hello
