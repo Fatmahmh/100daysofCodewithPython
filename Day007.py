492387b5aac5018045b399c1f2b3e9a002699def
ALharthi, Fatmah M <48739238+Fatmahmh@users.noreply.github.com>
2019-08-27 17:58:56 +0300

    Add files via upload

---
+++ Day007.py
@@ -0,0 +1,20 @@
+print("hello")
+print('hello')
+
+
+#Assign String to a Variable
+x = 'hello world'
+print(x)
+
+
+#Multiline Strings
+x = """hello world 
+this is day 007 in 100day of code
+with python """
+print(x)
+
+
+#Strings are Arrays
+x = 'hello world'
+print(x[1])
+print(x[2:5])
