e22d6eb76503bb7e6a657bdf948f433f2be56773
ALharthi, Fatmah M <48739238+Fatmahmh@users.noreply.github.com>
2019-08-27 17:59:15 +0300

    Add files via upload

---
+++ Day008.py
@@ -0,0 +1,11 @@
+x = ' hello world  '
+print(x.strip())   #The strip() method removes any whitespace from the beginning or the end.
+print(len(x))   # The len() method returns the length of a string.
+
+print(len(x.strip()))
+print(x.lower())  #The lower() method returns the string in lower case
+print(x.upper())  #The upper() method returns the string in upper case.
+
+print(x.replace("h" ,"j")) #The replace() method replaces a string with another string.
+print(x.split())  #The split() method splits the string into substrings 
+
