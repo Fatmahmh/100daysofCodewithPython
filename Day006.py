9428079a978f9aaf839cc7e2a6d4d11582ea0baa
ALharthi, Fatmah M <48739238+Fatmahmh@users.noreply.github.com>
2019-08-27 17:58:35 +0300

    Add files via upload

---
+++ Day006.py
@@ -0,0 +1,20 @@
+#int()
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x,y,z)
+
+
+#flat()
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.3")
+print(x,y,z,w)
+
+
+#str()
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x,y,z)
